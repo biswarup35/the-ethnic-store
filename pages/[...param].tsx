43f7d8046b7c6918ddf5cd514534c3d9401303b3
Biswarup35 <biswarup35@gmail.com>
2022-01-06 23:03:05 +0530

    generic page to handle errors

---
+++ pages/[...param].tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+interface GenericProps {}
+
+const Generic: React.FunctionComponent<GenericProps> = () => {
+  return <h1>This is generic page</h1>;
+};
+
+export default Generic;
