07efdd29c0eb2c6a50cf7b90269178277b10470d
Biswarup35 <biswarup35@gmail.com>
2022-01-19 19:50:14 +0530

    removing unnecessary files

--- pages/[...param].tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-interface GenericProps {}
-
-const Generic: React.FunctionComponent<GenericProps> = () => {
-  return <h1>This is generic page</h1>;
-};
-
-export default Generic;
