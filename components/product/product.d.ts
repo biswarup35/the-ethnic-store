c7d27f96fd8d77fc1efe7df7a520f873440a7111
Biswarup35 <biswarup35@gmail.com>
2022-01-19 19:53:40 +0530

    adding product type

---
+++ components/product/product.d.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: string;
+  for: string;
+  brand: string;
+  price: number;
+  discount: number;
+  size: string[];
+  rating: number;
+  image: string;
+  title: string;
+  description: string;
+}
